eb2740d21624bbb1b8d8476585cfbaf1161b0cd0
Qasim Khan <86830753+khan-qasim@users.noreply.github.com>
2022-06-19 18:06:24 +0400

    Update DBSCAN.py

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,6 +1,6 @@
 #Python code for the implementation of DBSCAN
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -26,19 +26,19 @@ df1 = df[df.cluster == -1]
 df2 = df[df.cluster == 0]
 #Figure size
 plt.figure(figsize = (9, 5))
-#plt.figure(plt.savefig(’DBSCAN_new.png’, format =
-’png’, dpi = 800))
+#plt.figure(plt.savefig("DBSCAN_new.png", format =
+"png", dpi = 800))
 plt.scatter(df1["Longitude"], df1["Latitude"],
 color = "red")
 plt.scatter(df2["Longitude"], df2["Latitude"],
 color = "green")
-plt.xlabel(’Longitude’)
-plt.ylabel(’Latitude’)
-plt.ticklabel_format(style = ’plain’)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.ticklabel_format(style = "plain")
 plt.legend()
 #plt.figure(plt.figure(figsize=(9, 5)))
 #plt.show()
-plt.savefig(’DBSCAN.png’, format=’png’, dpi=800)
+plt.savefig("DBSCAN.png", format="png", dpi=800)
 plt.close()
 eps_rng = range(0,5)
 mins = 3
@@ -54,7 +54,7 @@ distances,
 distances_d = pd.DataFrame({"distances": distances,
 "index": list(range(0, len(distances)))})
 plt.plot(distances_d.index, distances_d.distances)
-plt.xlabel(’Index’)
-plt.ylabel(’Distances’)
+plt.xlabel("Index")
+plt.ylabel("Distances")
 #plt.show()
-plt.savefig(’IndexvDist.png’, format=’png’, dpi=800)
+plt.savefig("IndexvDist.png", format="png", dpi=800)
